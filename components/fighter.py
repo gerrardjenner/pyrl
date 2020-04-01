3c43d2aa260d15709b879129e3765dc024b319ed
gerrardjenner <gerrardjenner@gmail.com>
2020-04-01 15:10:33 +1100

    adding hunger stats to fighter class

--- components/fighter.py
+++ components/fighter.py
@@ -4,13 +4,14 @@ from game_messages import Message
 
 
 class Fighter:
-    def __init__(self, hp, defense, power, xp=0, gold=0):
+    def __init__(self, hp, defense, power, xp=0, gold=0, hunger=0):
         self.base_max_hp = hp
         self.hp = hp
         self.base_defense = defense
         self.base_power = power
         self.xp = xp
         self.gold = gold
+        self.hunger = hunger
 
     @property
     def max_hp(self):
